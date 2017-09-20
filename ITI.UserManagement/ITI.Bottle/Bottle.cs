a8d66325c86d19a5677c0b30fa15f507cfbbf5e2
Olivier Spinelli <olivier.spinelli@invenietis.com>
2017-09-20 17:09:58 +0200

    Euuuh.... je ne suis pas allé bien loin: Slide 9 :D

---
+++ ITI.UserManagement/ITI.Bottle/Bottle.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ITI
+{
+    public class Bottle
+    {
+        readonly int _cap;
+        int _currentVolume;
+
+        /// <summary>
+        /// Initializes a new bottle with a capacity and
+        /// an initial volume.
+        /// All volumes are in milliliters.
+        /// </summary>
+        /// <param name="capacity">The bottle capacity.</param>
+        /// <param name="initialVolume">The initial volume.</param>
+        public Bottle(int capacity, int initialVolume)
+        {
+            _cap = capacity;
+            _currentVolume = initialVolume;
+        }
+
+        /// <summary>
+        /// Gets the capacity in milliliter.
+        /// </summary>
+        public int Capacity
+        {
+            get { return _cap; }
+        }
+
+        public int Volume
+        {
+            get { return _currentVolume; }
+            set { _currentVolume = value; }
+        }
+    }
+}
---
+++ ITI.UserManagement/ITI.Bottle/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ITI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var b = new Bottle(500, 133);
+
+            b.Volume = 54;
+
+            Console.WriteLine(b.Capacity);
+            Console.WriteLine(b.Volume);
+
+
+            //b.Capacity = 67;
+            b.Volume = 7676;
+
+            Console.ReadLine();
+        }
+    }
+}
