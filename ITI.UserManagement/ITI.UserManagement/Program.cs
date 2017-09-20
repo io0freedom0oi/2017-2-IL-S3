3d64c9ff27ef4fc66fa65c38e81b88f3d5a52cb7
Antoine Raquillet <raquillet@intechinfo.fr>
2017-09-20 10:17:17 +0200

    Added ITI.UserManagement.

---
+++ ITI.UserManagement/ITI.UserManagement/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ITI.UserManagement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User john = new User();
+            john.UserName = "John";
+            john.SetPassword("password");
+            Console.WriteLine("Password: ");
+            string password = Console.ReadLine();
+            if(john.PasswordMatch(password))
+            {
+                Console.WriteLine("Success !");
+            }
+            else
+            {
+                Console.WriteLine("Failure...");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ITI.UserManagement/ITI.UserManagement/User.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ITI.UserManagement
+{
+    public class User
+    {
+        string _userName;
+        string _password;
+
+        public string UserName
+        {
+            get { return _userName; }
+            set { _userName = value; }
+        }
+
+        public void SetPassword(string password)
+        {
+            _password = password;
+        }
+
+        public bool PasswordMatch(string candidate)
+        {
+            return candidate == _password;
+        }
+    }
+}
