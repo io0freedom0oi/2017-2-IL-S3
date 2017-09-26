ec7b31f12cbef0c84b04745c1691d44f2541287e
Antoine Raquillet <raquillet@intechinfo.fr>
2017-09-26 10:31:41 +0200

    Added ITI.AccountManagement.

---
+++ ITI.AccountManagement/ITI.AccountManagement.UI/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using ITI.AccountManagement;
+
+namespace ITI.AccountManagement.UI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account account = new Account("845236658", "0123", 5000);
+            int balance = account.GetBalance("0123");
+            Console.WriteLine("Balance: {0}", balance);
+
+            bool success = account.Debit("0123", 2000);
+            balance = account.GetBalance("0123");
+            Console.WriteLine("Balance: {0}", balance);
+
+            account.Credit(1000);
+            balance = account.GetBalance("0123");
+            Console.WriteLine("Balance: {0}", balance);
+
+            success = account.ChangePassword("0123", "4321");
+            balance = account.GetBalance("0123");
+            Console.WriteLine("Balance: {0}", balance);
+            balance = account.GetBalance("4321");
+            Console.WriteLine("Balance: {0}", balance);
+        }
+    }
+}
---
+++ ITI.AccountManagement/ITI.AccountManagement/Account.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace ITI.AccountManagement
+{
+    public class Account
+    {
+        readonly string _accountId;
+        string _password;
+        int _balance;
+
+        public Account(string accountId, string password, int balance)
+        {
+            _accountId = accountId;
+            _password = password;
+            _balance = balance;
+        }
+
+        public int GetBalance(string password)
+        {
+            if (_password != password) return -1;
+            return _balance;
+
+            // return _password == password ? _balance : -1;
+        }
+
+        public bool Debit(string password, int amount)
+        {
+            if (password != _password || amount > _balance) return false;
+            _balance -= amount;
+            return true;
+        }
+
+        public bool Credit(int amount)
+        {
+            if (amount < 0) return false;
+            _balance += amount;
+            return true;
+        }
+
+        public bool ChangePassword(string currentPassword, string newPassword)
+        {
+            if (currentPassword != _password) return false;
+            _password = newPassword;
+            return true;
+        }
+    }
+}
