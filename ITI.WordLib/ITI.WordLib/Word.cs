9b6a588b17670bba7636832d4a9798cf58ff55d8
Antoine Raquillet <raquillet@intechinfo.fr>
2017-09-28 10:10:44 +0200

    Enahnced ITI.WordLib.

--- ITI.WordLib/ITI.WordLib/Word.cs
+++ ITI.WordLib/ITI.WordLib/Word.cs
@@ -6,19 +6,40 @@ namespace ITI.WordLib
     {
         readonly string _word;
 
-        public Word( string word )
+        public Word(string word)
         {
+            if (string.IsNullOrWhiteSpace(word)) throw new ArgumentException("The word must be nt null nor whitespace.", nameof(word));
+            WordGuard(word);
+
             _word = word;
+        }
+
+        static void WordGuard(string word)
+        {
+            for (int idx = 0; idx < word.Length; idx++)
+            {
+                char c = word[idx];
+                if (!char.IsLetter(c)) throw new ArgumentException("A word must contain only letters.", nameof(word));
+            }
         }
 
         public bool IsPalindrome
         {
-            get { throw new NotImplementedException(); }
+            get
+            {
+                string value = Value;
+                for (int idx = 0; idx < value.Length / 2; idx++)
+                {
+                    if (value[idx] != value[value.Length - (1 + idx)]) return false;
+                }
+
+                return true;
+            }
         }
 
         public int Length
         {
-            get { throw new NotImplementedException(); }
+            get { return _word.Length; }
         }
 
         public string Original
@@ -28,20 +49,29 @@ namespace ITI.WordLib
 
         public string Value
         {
-            get { throw new NotImplementedException(); }
+            get { return _word.ToLower(); }
         }
 
-        public int GetCharacterCount( char c )
+        public int GetCharacterCount(char c)
+        {
+            string value = Value;
+            int count = 0;
+            c = char.ToLower(c);
+
+            foreach (char current in value)
+            {
+                if (current == c) count++;
+            }
+
+            return count;
+        }
+
+        public bool IsRhymingWith(Word candidate)
         {
             throw new NotImplementedException();
         }
 
-        public bool IsRhymingWith( Word candidate )
-        {
-            throw new NotImplementedException();
-        }
-
-        public bool IsAnagramOf( Word candidate )
+        public bool IsAnagramOf(Word candidate)
         {
             throw new NotImplementedException();
         }
