a31421af8b741e71b1b7702f2f553f90935a38b5
Antoine Raquillet <raquillet@intechinfo.fr>
2017-09-29 12:02:50 +0200

    Added (not implemented) LinkedListInt.

---
+++ ITI.Collections/ITI.Collections/ItemInt.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITI.Collections
+{
+    class ItemInt
+    {
+        int _value;
+        ItemInt _next;
+
+        public ItemInt(int value, ItemInt next)
+        {
+            _value = value;
+            _next = next;
+        }
+
+        public int Value
+        {
+            get { return _value; }
+            set { _value = value; }
+        }
+
+        public ItemInt Next
+        {
+            get { return _next; }
+            set { _next = value; }
+        }
+    }
+}
---
+++ ITI.Collections/ITI.Collections/LinkedListInt.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ITI.Collections
+{
+    public class LinkedListInt
+    {
+        ItemInt _first;
+
+        public void Add(int value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetAt(int index)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
