affd69b8a38ff35d9fd02cf087bad567b018d68c
Antoine Raquillet <raquillet@intechinfo.fr>
2017-10-02 09:48:50 +0200

    Added ITI.Zoo.

---
+++ ITI.Zoo/ITI.Zoo.Tests/SimpleTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace ITI.Zoo.Tests
+{
+    [TestFixture]
+    public class SimpleTests
+    {
+        [Test]
+        public void ShouldPass()
+        {
+            Assert.That(0, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void ShouldFail()
+        {
+            Assert.That(1, Is.EqualTo(0));
+        }
+    }
+}
